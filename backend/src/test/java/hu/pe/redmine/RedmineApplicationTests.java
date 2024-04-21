299a333e9b8c1b93f4a3ae359a1547232f2bc1a4
Bendegúz Kiss <kissbendeguz12@gmail.com>
2024-04-21 21:21:46 +0200

    Initial commit

---
+++ backend/src/test/java/hu/pe/redmine/RedmineApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.pe.redmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedmineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
